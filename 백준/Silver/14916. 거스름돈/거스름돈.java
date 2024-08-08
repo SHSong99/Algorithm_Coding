9746c9503fd73060bea1450bac04a48b5efb4ee5
송성현 <161740297+SHSong99@users.noreply.github.com>
2024-08-08 10:58:22 +0900

    [Silver V] Title: 거스름돈, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -4,29 +4,43 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int mon = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 2) {
+            System.out.println(1);
+            return;
+        } else if (N == 4) {
+            System.out.println(2);
+            return;
+        }
+
+        if (N % 5 == 0) {
+            System.out.println(N / 5);
+            return;
+        } else if (N % 5 == 2) {
+            System.out.println((N / 5) + 1);
+            return;
+        } else if (N % 5 == 4) {
+            System.out.println((N / 5) + 2);
+            return;
+        }
 
         int count = 0;
 
         while (true) {
-            if (mon % 5 == 0) {
-                count += mon / 5;
-                System.out.println(count);
-                break;
-            } else {
-                mon -= 2;
-                count++;
-                if (mon == 0) {
-                    System.out.println(count);
-                    break;
-                } else if (mon <= -1) {
-                    System.out.println("-1");
-                    break;
-                }
+            if (N == 6) {
+                System.out.println(count + 3);
+                return;
+            } else if (N == 8) {
+                System.out.println(count + 4);
+                return;
+            } else if (N <= 0) {
+                System.out.println(-1);
+                return;
             }
+            N -= 5;
+            count++;
         }
-
     }
 }
